08804fbd2646776e57e11a1a6931ed62bda99e2e
hnooz <mohammededris23@gmail.com>
2026-04-07 00:02:00 +0300

    feat: Enable login for admin panel in AdminPanelProvider

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -26,6 +26,7 @@ class AdminPanelProvider extends PanelProvider
             ->default()
             ->id('admin')
             ->path('admin')
+            ->login()
             ->brandName('HomeDuty Admin')
             ->colors([
                 'primary' => Color::Indigo,
